303a34632e86c06885c7170073e4ccd2a0eb75b3
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-12-07 08:36:34 -0300

    wip

--- AnkiDroid/src/main/java/com/ichi2/anki/preferences/ReviewerMenuSettingsFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/preferences/ReviewerMenuSettingsFragment.kt
@@ -15,7 +15,113 @@
  */
 package com.ichi2.anki.preferences
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.MaterialToolbar
 import com.ichi2.anki.R
+import com.ichi2.anki.preferences.reviewer.MenuDisplayType
+import com.ichi2.anki.preferences.reviewer.OnClearViewListener
+import com.ichi2.anki.preferences.reviewer.ReviewerMenuSettingsAdapter
+import com.ichi2.anki.preferences.reviewer.ReviewerMenuSettingsRecyclerItem
+import com.ichi2.anki.preferences.reviewer.ReviewerMenuSettingsTouchHelperCallback
+import com.ichi2.anki.preferences.reviewer.ViewerAction
+import com.ichi2.anki.utils.ext.sharedPrefs
 
-class ReviewerMenuSettingsFragment : Fragment(R.layout.preferences_reviewer_menu)
+class ReviewerMenuSettingsFragment :
+    Fragment(R.layout.preferences_reviewer_menu),
+    OnClearViewListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuItems = MenuDisplayType.getMenuItems(sharedPrefs())
+        setupRecyclerView(view, menuItems)
+
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        val menu = toolbar.menu
+        (menu as? MenuBuilder)?.setOptionalIconsVisible(true)
+        setupMenu(toolbar.menu, menuItems)
+    }
+
+    private fun setupRecyclerView(view: View, menuItems: MenuDisplayType.Items) {
+        fun toRecyclerItems(items: List<ViewerAction>): Array<ReviewerMenuSettingsRecyclerItem> =
+            items.map { ReviewerMenuSettingsRecyclerItem.Action(it) }.toTypedArray()
+
+        val recyclerViewItems = listOf(
+            ReviewerMenuSettingsRecyclerItem.DisplayType(MenuDisplayType.ALWAYS),
+            *toRecyclerItems(menuItems.alwaysShow),
+            ReviewerMenuSettingsRecyclerItem.DisplayType(MenuDisplayType.MENU_ONLY),
+            *toRecyclerItems(menuItems.menuOnly),
+            ReviewerMenuSettingsRecyclerItem.DisplayType(MenuDisplayType.DISABLED),
+            *toRecyclerItems(menuItems.disabled)
+        )
+
+        val callback = ReviewerMenuSettingsTouchHelperCallback(recyclerViewItems)
+        callback.setOnClearViewListener(this)
+        val itemTouchHelper = ItemTouchHelper(callback)
+
+        val adapter = ReviewerMenuSettingsAdapter(recyclerViewItems).apply {
+            setOnDragListener { viewHolder ->
+                itemTouchHelper.startDrag(viewHolder)
+            }
+        }
+        view.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            this.adapter = adapter
+            itemTouchHelper.attachToRecyclerView(this)
+        }
+    }
+
+    override fun onClearView(items: List<ReviewerMenuSettingsRecyclerItem>) {
+        val menuOnlyItemsIndex = items.indexOfFirst {
+            it is ReviewerMenuSettingsRecyclerItem.DisplayType && it.menuDisplayType == MenuDisplayType.MENU_ONLY
+        }
+        val disabledItemsIndex = items.indexOfFirst {
+            it is ReviewerMenuSettingsRecyclerItem.DisplayType && it.menuDisplayType == MenuDisplayType.DISABLED
+        }
+
+        val alwaysShowItems = items.subList(1, menuOnlyItemsIndex)
+            .mapNotNull { (it as? ReviewerMenuSettingsRecyclerItem.Action)?.viewerAction }
+        val onMenuItems = items.subList(menuOnlyItemsIndex, disabledItemsIndex)
+            .mapNotNull { (it as? ReviewerMenuSettingsRecyclerItem.Action)?.viewerAction }
+        val disabledItems = items.subList(disabledItemsIndex, items.lastIndex)
+            .mapNotNull { (it as? ReviewerMenuSettingsRecyclerItem.Action)?.viewerAction }
+
+        val preferences = sharedPrefs()
+        MenuDisplayType.ALWAYS.setPreferenceValue(preferences, alwaysShowItems)
+        MenuDisplayType.MENU_ONLY.setPreferenceValue(preferences, onMenuItems)
+        MenuDisplayType.DISABLED.setPreferenceValue(preferences, disabledItems)
+    }
+
+    private fun addActionsToMenu(
+        menu: Menu,
+        actions: List<ViewerAction>,
+        menuActionType: Int
+    ) {
+        val menuActions = ViewerAction.entries.mapNotNull { it.parentMenu }
+        for (action in actions) {
+            val title = action.titleRes?.let { getString(it) } ?: ""
+            val menuItem = if (action in menuActions) {
+                menu.addSubMenu(Menu.NONE, action.menuId, Menu.NONE, title)
+                menu.findItem(action.menuId)
+            } else {
+                menu.add(Menu.NONE, action.menuId, Menu.NONE, title)
+            }
+            with(menuItem) {
+                action.drawableRes?.let { setIcon(it) }
+                setShowAsAction(menuActionType)
+            }
+        }
+    }
+
+    private fun setupMenu(menu: Menu, menuItems: MenuDisplayType.Items) {
+        addActionsToMenu(menu, menuItems.alwaysShow, MenuItem.SHOW_AS_ACTION_ALWAYS)
+        addActionsToMenu(menu, menuItems.menuOnly, MenuItem.SHOW_AS_ACTION_NEVER)
+    }
+}
--- AnkiDroid/src/main/java/com/ichi2/anki/preferences/reviewer/MenuDisplayType.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/preferences/reviewer/MenuDisplayType.kt
@@ -15,7 +15,9 @@
  */
 package com.ichi2.anki.preferences.reviewer
 
+import android.content.SharedPreferences
 import androidx.annotation.StringRes
+import androidx.core.content.edit
 import com.ichi2.anki.R
 
 enum class MenuDisplayType(@StringRes val title: Int) {
@@ -24,4 +26,55 @@ enum class MenuDisplayType(@StringRes val title: Int) {
     DISABLED(R.string.disabled);
 
     private val preferenceKey get() = "ReviewerMenuDisplayType_$name"
+
+    /**
+     * @return the configured actions for this menu display type.
+     */
+    private fun getConfiguredActions(preferences: SharedPreferences): List<ViewerAction> {
+        val prefValue = preferences.getString(preferenceKey, null)
+            ?: return emptyList()
+
+        val actionsNames = prefValue.split(SEPARATOR)
+        return actionsNames.mapNotNull { name ->
+            ViewerAction.entries.firstOrNull { it.name == name }
+        }
+    }
+
+    fun setPreferenceValue(preferences: SharedPreferences, actions: List<ViewerAction>) {
+        val prefValue = actions.joinToString(SEPARATOR) { it.name }
+        preferences.edit { putString(preferenceKey, prefValue) }
+    }
+
+    data class Items(
+        val alwaysShow: List<ViewerAction>,
+        val menuOnly: List<ViewerAction>,
+        val disabled: List<ViewerAction>
+    )
+
+    companion object {
+        private const val SEPARATOR = ","
+
+        /**
+         * @return actions that weren't configured yet. May happen if the user hasn't configured
+         * any of the menu actions, or if a new action was implemented but not configured yet.
+         */
+        private fun getAllNotConfiguredActions(prefs: SharedPreferences): List<ViewerAction> {
+            val mappedActions = MenuDisplayType.entries.flatMap { it.getConfiguredActions(prefs) }
+            return ViewerAction.entries.filter {
+                it.defaultDisplayType != null && it !in mappedActions
+            }
+        }
+
+        fun getMenuItems(prefs: SharedPreferences): Items {
+            val notConfiguredActions = getAllNotConfiguredActions(prefs)
+            val alwaysShowActions = ALWAYS.getConfiguredActions(prefs) +
+                notConfiguredActions.filter { it.defaultDisplayType == ALWAYS }
+            val menuOnlyActions = MENU_ONLY.getConfiguredActions(prefs) +
+                notConfiguredActions.filter { it.defaultDisplayType == MENU_ONLY }
+            val disabledActions = DISABLED.getConfiguredActions(prefs) +
+                notConfiguredActions.filter { it.defaultDisplayType == DISABLED }
+
+            return Items(alwaysShowActions, menuOnlyActions, disabledActions)
+        }
+    }
 }
