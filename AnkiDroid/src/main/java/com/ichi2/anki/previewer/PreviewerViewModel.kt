f0aba071ef7d06d1f54c3ea8441a809d9d284fe8
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-12-21 12:10:33 -0300

    feat(previewers): non combining typed answers
    Original PR and motivation: https://github.com/ankitects/anki/pull/3422#issue-2528848420
    
    Diff: https://github.com/ankitects/anki/pull/3422/files#diff-7c3df145bba194e3cdb8114cb4bd77c03e431c29c82a7620b4f4df599cd4b450
    
    The implementation is also being refactored into a class to better handle all of the different details around type in answer, e.g. combining, expected answer and language hint

--- AnkiDroid/src/main/java/com/ichi2/anki/previewer/PreviewerViewModel.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/previewer/PreviewerViewModel.kt
@@ -236,9 +236,10 @@ class PreviewerViewModel(
     /** From the [desktop code](https://github.com/ankitects/anki/blob/1ff55475b93ac43748d513794bcaabd5d7df6d9d/qt/aqt/reviewer.py#L671) */
     override suspend fun typeAnsFilter(text: String): String =
         if (showingAnswer.value) {
-            typeAnsAnswerFilter(currentCard.await(), text)
+            val typeInAnswer = TypeInAnswer.getInstance(currentCard.await(), text)
+            typeInAnswer?.answerFilter() ?: text
         } else {
-            removeTypeInAnswerTags(text)
+            TypeInAnswer.removeTypeAnswerTags(text)
         }
 
     companion object {
--- AnkiDroid/src/main/java/com/ichi2/anki/previewer/TypeInAnswer.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/previewer/TypeInAnswer.kt
@@ -15,81 +15,100 @@
  */
 package com.ichi2.anki.previewer
 
+import android.os.LocaleList
 import com.ichi2.anki.CollectionManager.withCol
+import com.ichi2.anki.servicelayer.LanguageHintService
 import com.ichi2.libanki.Card
 import org.intellij.lang.annotations.Language
 import org.jetbrains.annotations.VisibleForTesting
 import org.json.JSONObject
 
-// Desktop source: https://github.com/ankitects/anki/blob/1ff55475b93ac43748d513794bcaabd5d7df6d9d/qt/aqt/reviewer.py#L669
+// Adapted from https://github.com/ankitects/anki/blob/8af63f81eb235b8d21df4e8eeaa6e02f46b3fbf6/qt/aqt/reviewer.py
+//  and https://github.com/ankitects/anki/blob/df70564079f53e587dc44f015c503fdf6a70924f/qt/aqt/clayout.py#L579
+class TypeInAnswer private constructor(
+    private val text: String,
+    private val combining: Boolean,
+    val field: JSONObject,
+    var expectedAnswer: String,
+) {
+    val imeHintLocales: LocaleList? by lazy {
+        LanguageHintService.getImeHintLocales(this.field)
+    }
 
-@VisibleForTesting
-val typeAnsRe = Regex("\\[\\[type:(.+?)]]")
+    suspend fun answerFilter(typedAnswer: String = ""): String {
+        val typeFont = field.getString("font")
+        val typeSize = field.getString("size")
+        val answerComparison = withCol { compareAnswer(expectedAnswer, provided = typedAnswer, combining = combining) }
 
-/** removes `[[type:]]` tags */
-@VisibleForTesting
-fun removeTypeInAnswerTags(text: String) = typeAnsRe.replace(text, "")
+        @Language("HTML")
+        val repl = """<div style="font-family: '$typeFont'; font-size: ${typeSize}px">$answerComparison</div>"""
+        return typeAnsRe.replace(text, repl)
+    }
 
-suspend fun getTypeAnswerField(
-    card: Card,
-    text: String,
-): JSONObject? {
-    val match = typeAnsRe.find(text) ?: return null
+    companion object {
+        /** removes `[[type:]]` tags */
+        fun removeTypeAnswerTags(text: String): String = typeAnsRe.replace(text, "")
 
-    val typeAnsFieldName =
-        match.groups[1]!!.value.let {
-            if (it.startsWith("cloze:")) {
-                it.split(":")[1]
+        private fun getTypeAnswerTag(text: String): String? {
+            val match = typeAnsRe.find(text) ?: return null
+            return match.groups[1]?.value
+        }
+
+        private suspend fun getExpectedTypeInAnswer(
+            card: Card,
+            field: JSONObject,
+        ): String {
+            val fieldName = field.getString("name")
+            val expected = withCol { card.note(this@withCol).getItem(fieldName) }
+            return if (fieldName.startsWith("cloze:")) {
+                val clozeIdx = card.ord + 1
+                withCol {
+                    extractClozeForTyping(expected, clozeIdx)
+                }
             } else {
-                it
+                expected
             }
         }
 
-    val fields = withCol { card.noteType(this).flds }
-    for (i in 0 until fields.length()) {
-        val field = fields.get(i) as JSONObject
-        if (field.getString("name") == typeAnsFieldName) {
-            return field
+        suspend fun getInstance(
+            card: Card,
+            text: String,
+        ): TypeInAnswer? {
+            val tag = getTypeAnswerTag(text) ?: return null
+
+            val combining = !tag.startsWith("nc:")
+            val typeAnsFieldName =
+                tag.let { field ->
+                    val prefix = field.substringBefore(":")
+                    when (prefix) {
+                        "cloze",
+                        "nc",
+                        -> field.substringAfter(":")
+                        else -> field
+                    }
+                }
+
+            val fields = withCol { card.noteType(this).flds }
+            var typeAnswerField: JSONObject? = null
+            for (i in 0 until fields.length()) {
+                val field = fields.get(i) as JSONObject
+                if (field.getString("name") == typeAnsFieldName) {
+                    typeAnswerField = field
+                    break
+                }
+            }
+            if (typeAnswerField == null) return null
+            val expectedAnswer = getExpectedTypeInAnswer(card, typeAnswerField)
+
+            return TypeInAnswer(
+                text = text,
+                combining = combining,
+                field = typeAnswerField,
+                expectedAnswer = expectedAnswer,
+            )
         }
     }
-    return null
 }
 
-suspend fun getExpectedTypeInAnswer(
-    card: Card,
-    field: JSONObject,
-): String? {
-    val fieldName = field.getString("name")
-    val expected = withCol { card.note(this@withCol).getItem(fieldName) }
-    return if (fieldName.startsWith("cloze:")) {
-        val clozeIdx = card.ord + 1
-        withCol {
-            extractClozeForTyping(expected, clozeIdx).takeIf { it.isNotBlank() }
-        }
-    } else {
-        expected
-    }
-}
-
-fun getFontSize(field: JSONObject): String = field.getString("size")
-
-/** Adapted from the [desktop code](https://github.com/ankitects/anki/blob/1ff55475b93ac43748d513794bcaabd5d7df6d9d/qt/aqt/reviewer.py#L720) */
-suspend fun typeAnsAnswerFilter(
-    card: Card,
-    text: String,
-): String {
-    val typeAnswerField =
-        getTypeAnswerField(card, text)
-            ?: return typeAnsRe.replace(text, "")
-    val expectedAnswer =
-        getExpectedTypeInAnswer(card, typeAnswerField)
-            ?: return typeAnsRe.replace(text, "")
-    val typeFont = typeAnswerField.getString("font")
-    val typeSize = getFontSize(typeAnswerField)
-    val answerComparison = withCol { compareAnswer(expectedAnswer, provided = "") }
-
-    @Language("HTML")
-    val output =
-        """<div style="font-family: '$typeFont'; font-size: ${typeSize}px">$answerComparison</div>"""
-    return typeAnsRe.replace(text, output)
-}
+@VisibleForTesting
+val typeAnsRe = Regex("\\[\\[type:(.+?)]]")
