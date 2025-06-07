71d1f64221501eb476093e366644d1b1540d5eeb
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2025-06-07 13:42:30 -0300

    feat(new reviewer): tap gestures
    Implements tap gestures in the new reviewer. Swipe gestures will be implemented later.
    
    The tap detection is done in JavaScript so we can avoid triggering the gesture when tapping buttons, links, etc
    
    The `isLink` method was shared by Damien (I can't find the original link) https://github.com/ankidroid/Anki-Android/issues/14303#issuecomment-1693696444
    
    Only the `9 point touch mode` is supported because I think that the 4 points mode became unnecessary after the interactable gesture selection

--- AnkiDroid/src/main/assets/scripts/ankidroid.js
+++ AnkiDroid/src/main/assets/scripts/ankidroid.js
@@ -21,3 +21,75 @@ globalThis.ankidroid.showHint = function () {
 globalThis.ankidroid.showAllHints = function () {
     document.querySelectorAll("a.hint").forEach(el => el.click());
 };
+
+(() => {
+    let startX = 0,
+        startY = 0,
+        tapTimer = null,
+        isSingleTouch = false;
+
+    document.ontouchstart = function (event) {
+        if (event.touches.length > 1) {
+            isSingleTouch = false;
+            return;
+        }
+        startX = event.touches[0].clientX;
+        startY = event.touches[0].clientY;
+        scrollX = window.scrollX;
+        scrollY = window.scrollY;
+        isSingleTouch = true;
+    };
+
+    document.ontouchend = function (event) {
+        if (!isSingleTouch || isTextSelected() || isLink(event)) return;
+
+        let endX = event.changedTouches[0].clientX,
+            endY = event.changedTouches[0].clientY;
+
+        if (tapTimer != null) {
+            window.location.href = "tap://double";
+            event.preventDefault();
+            clearTimeout(tapTimer);
+            tapTimer = null;
+            return;
+        }
+
+        tapTimer = setTimeout(() => {
+            const params = new URLSearchParams({
+                x: endX,
+                y: endY,
+                deltaX: Math.abs(endX - startX),
+                deltaY: Math.abs(endY - startY),
+            });
+            window.location.href = `tap://single/?${params.toString()}`;
+            event.preventDefault();
+            tapTimer = null;
+        }, 200);
+    };
+
+    function isLink(e) {
+        let node = e.target;
+        while (node && node !== document) {
+            const res =
+                node.nodeName === "A" ||
+                node.onclick ||
+                node.nodeName === "BUTTON" ||
+                node.nodeName === "VIDEO" ||
+                node.nodeName === "SUMMARY" ||
+                node.nodeName === "INPUT" ||
+                node.getAttribute("contentEditable");
+            if (res) {
+                return true;
+            }
+            if (node.classList && node.classList.contains("tappable")) {
+                return true;
+            }
+            node = node.parentNode;
+        }
+        return false;
+    }
+
+    function isTextSelected() {
+        return !document.getSelection().isCollapsed;
+    }
+})();
